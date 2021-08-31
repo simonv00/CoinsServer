bafbaaebf40ecd2312c6b699bff009109fc7bfb7
Cristina Arango Escobar <cristinarangoe@hotmail.com>
2021-08-31 10:34:40 -0500

    Se hizo el listar

--- MVC/EstudianteDAO.js
+++ MVC/EstudianteDAO.js
@@ -3,14 +3,17 @@ import mySql from 'mysql'
 const db = mySql.createPool({
     host: 'localhost' ,
     user: 'root' ,
-    password: '011298' ,
-    database: 'coins'
+    password: '9908' ,
+    database: 'Coins'
 });
 
 export default function insertStudent(estudiante){
     const sqlInsert = "INSERT INTO estudiantes (Numero_Documento, Primer_Nombre, Segundo_Nombre, Primer_Apellido, Segundo_Apellido, Celular, Tipo_Documento, Correo, Saldo, Estado) VALUES (?,?,?,?,?,?,?,?,?,?)"
     const {id,nombre1, nombre2, apellido1, apellido2, telefono,tipoID, correo,  monto, estado} = estudiante
     db.query(sqlInsert, [id,nombre1, nombre2, apellido1, apellido2, telefono,tipoID, correo,  monto, estado] , (err, result) => {
+        if(err){
+            throw err
+        }
         console.log(result)
     })
     
--- index.js
+++ index.js
@@ -3,14 +3,36 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import controlador from './MVC/Controlador.js'
 const app = express()
+import mySql from 'mysql'
 
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-    app.post('/Registro/api/insert', (req,res) => {
-    const {Numero_Documento, Primer_Nombre, Segundo_Nombre, Primer_Apellido, Segundo_Apellido, Celular, Tipo_Documento, Correo, Saldo, Estado} = req.body
-    controlador(Numero_Documento, Primer_Nombre, Segundo_Nombre, Primer_Apellido, Segundo_Apellido, Celular, Tipo_Documento, Correo, Saldo, Estado)
+const db = mySql.createPool({
+    host: 'localhost' ,
+    user: 'root' ,
+    password: '9908' ,
+    database: 'Coins'
+});
+
+app.get('/Listar/api/get', (req, res) => {
+    const sqlInsert = "SELECT * FROM estudiantes"
+    db.query(sqlInsert, (err, result) => {
+        res.send(result)
+    })
+})
+
+app.post('/Registro/api/insert', (req,res) => {
+    try {
+        const {Numero_Documento, Primer_Nombre, Segundo_Nombre, Primer_Apellido, Segundo_Apellido, Celular, Tipo_Documento, Correo, Saldo, Estado} = req.body
+        controlador(Numero_Documento, Primer_Nombre, Segundo_Nombre, Primer_Apellido, Segundo_Apellido, Celular, Tipo_Documento, Correo, Saldo, Estado)
+        return res.send("Succesfully inserted").status(200)
+    } catch (error) {
+        //throw error
+        console.log(error)
+        return res.send("bad request").status(400)
+    }
 })
 
 app.listen(3001,()=>{
