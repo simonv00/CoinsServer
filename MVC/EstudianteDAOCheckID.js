351baf3586aff3262ecd2030583a95b4d7839dbd
Simon Valencia Ochoa <simonvalencia305@gmail.com>
2021-11-02 13:01:22 -0500

    Create EstudianteDAOCheckID.js

---
+++ MVC/EstudianteDAOCheckID.js
@@ -0,0 +1,29 @@
+/* import mySql from "mysql"; */
+
+const mySql = require('mysql')
+
+// conexion con la base de datos
+const db = mySql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "pinguino",
+  database: "Coins",
+});
+
+
+module.exports = async function checkStudentID(montoObj,AllGood, callback) {
+
+    if (!AllGood) return callback(false) // esta opcion verifica que todas las validfaciones esten correctas
+    const sqlExists = "SELECT * from estudiantes WHERE Numero_Documento= (?)";
+    const {id, monto, tipoID} = montoObj;
+    console.log("ID: ", id)
+    db.query(sqlExists, [id], (err, result) => {
+      if (result.length == 0) {
+        // El dato no esta en la base de datos
+        return callback(false);
+      } else {
+        //hay un error, probablemente un dato duplicado
+        return callback(true);
+      }
+    });
+  }
