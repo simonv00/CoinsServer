c55f1f8db71a989a47429e711561a79cfb515b26
Simon Valencia Ochoa <simonvalencia305@gmail.com>
2021-11-02 13:01:18 -0500

    Create EstudianteDAOCheck.js

---
+++ MVC/EstudianteDAOCheck.js
@@ -0,0 +1,29 @@
+/* import mySql from "mysql"; */
+
+const mySql = require('mysql')
+
+// conexion con la base de datos
+const db = mySql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "pinguino",
+  database: "Coins",
+});
+
+module.exports = async function checkStudent(estudiante,AllGood, callback) {
+
+    if (!AllGood) return callback(false) // esta opcion verifica que todas las validfaciones esten correctas
+    const sqlExists = "SELECT * from estudiantes WHERE Numero_Documento= (?) OR Correo = (?)";
+    const {id,nombre1,nombre2,apellido1,apellido2,telefono,tipoID,correo,monto,estado, esEstudiante,carrera} = estudiante;
+  
+    db.query(sqlExists, [id, correo], (err, result) => {
+      console.log(result)
+      if (result.length == 0) {
+        // El dato no esta en la base de datos
+        return callback(true);
+      } else {
+        //hay un error, probablemente un dato duplicado
+        return callback(false);
+      }
+    });
+  }
