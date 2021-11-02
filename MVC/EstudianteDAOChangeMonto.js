356d4967cc93a447e81070061b0fd0515904c288
Simon Valencia Ochoa <simonvalencia305@gmail.com>
2021-11-02 13:01:14 -0500

    Create EstudianteDAOChangeMonto.js

---
+++ MVC/EstudianteDAOChangeMonto.js
@@ -0,0 +1,30 @@
+/* import mySql from "mysql"; */
+
+const mySql = require('mysql')
+
+// conexion con la base de datos
+const db = mySql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "pinguino",
+  database: "Coins",
+});
+
+module.exports = async function changeMonto(montoObj, AllGood, callback) {
+
+    if (!AllGood) return callback(false) // esta opcion verifica que todas las validfaciones esten correctas
+    const sqlSaldo = "UPDATE estudiantes SET Saldo =  Saldo + (?) WHERE Numero_Documento = (?)";
+    //const sqlSum = "UPDATE estudiantes SET Saldo =  (?) + (?) WHERE Numero_Documento = (?)";
+    const {id, monto, tipoID} = montoObj;
+  
+    db.query(sqlSaldo, [monto, id],(err,result) => {
+      //console.log(result)
+      if (err) {
+        //console.log(err)
+        return callback(false)
+      }
+      else{
+        return callback(true)
+      }
+    });
+}
