28e8fde46be13a602689e698cbaa569d1b7ce852
Simon Valencia Ochoa <simonvalencia305@gmail.com>
2021-11-03 16:34:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const mySql = require('mysql')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({ origin: "https://coins-interfaz.herokuapp.com/", credentials: true }))
+app.use(cors({ origin: "*", credentials: true }))
+
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
