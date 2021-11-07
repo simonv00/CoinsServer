e952136b997ec3dad33b41cc0ccbbbbd67f5fe68
Simon Valencia Ochoa <simonvalencia305@gmail.com>
2021-11-06 23:38:34 -0500

    Create ControladorPlaca.js

---
+++ MVC/ControladorPlaca.js
@@ -0,0 +1,11 @@
+const PlacaObj = require('./PlacaObj')
+const validacion = require('./EstudianteBO3Placa')
+
+module.exports = function controladorMonto(Numero_Documento, Tipo_Documento, Placa,callback) {
+    const placaObj = new PlacaObj(Numero_Documento, Tipo_Documento, Placa)
+    console.log('placa obj: ',placaObj)
+
+    return validacion.validatePlaca(placaObj,(value)=>{
+        return callback(value)
+    })
+}
