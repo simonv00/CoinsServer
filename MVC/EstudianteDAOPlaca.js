62b164a3086393c77ad91dec9b3c4b8fef8c9eb5
Simon Valencia Ochoa <simonvalencia305@gmail.com>
2021-11-06 23:38:39 -0500

    Create EstudianteDAOPlaca.js

---
+++ MVC/EstudianteDAOPlaca.js
@@ -0,0 +1,33 @@
+/* import mySql from "mysql"; */
+
+const mySql = require('mysql')
+
+// conexion con la base de datos
+const db = mySql.createPool({
+  /*host: "localhost",
+  user: "root",
+  password: "pinguino",
+  database: "Coins",*/
+  database: 'sql10448835',
+  user: 'sql10448835',
+  host: 'sql10.freemysqlhosting.net',
+  password: 'ssU2dhtbJz',
+});
+
+module.exports = async function agregarPlaca(placaObj, AllGood, callback) {
+
+    if (!AllGood) return callback(false) // esta opcion verifica que todas las validfaciones esten correctas
+    const sqlPlaca = "INSERT INTO Placas (Numero_Documento,Placa) VALUES (?,?)";
+    const {id, placa, tipoID} = placaObj;
+  
+    db.query(sqlPlaca, [id, placa],(err,result) => {
+      //console.log(result)
+      if (err) {
+        //console.log(err)
+        return callback(false)
+      }
+      else{
+        return callback(true)
+      }
+    });
+}
