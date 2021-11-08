2427241c290f9a4522da368692e893aca0d7868e
Simon Valencia Ochoa <simonvalencia305@gmail.com>
2021-11-07 20:50:42 -0500

    Update ControladorPlaca.js

--- MVC/ControladorPlaca.js
+++ MVC/ControladorPlaca.js
@@ -4,7 +4,7 @@ const validacion = require('./EstudianteBO3Placa')
 module.exports = function controladorMonto(Numero_Documento, Tipo_Documento, Placa,callback) {
     const placaObj = new PlacaObj(Numero_Documento, Tipo_Documento, Placa)
     console.log('placa obj: ',placaObj)
-
+    console.log("hola")
     return validacion.validatePlaca(placaObj,(value)=>{
         return callback(value)
     })
