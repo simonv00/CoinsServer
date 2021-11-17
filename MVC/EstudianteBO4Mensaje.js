30a57c1a451f2898082998a203b4322c07e1c29c
Simon Valencia Ochoa <simonvalencia305@gmail.com>
2021-11-17 16:55:05 -0500

    Bug

--- MVC/EstudianteBO4Mensaje.js
+++ MVC/EstudianteBO4Mensaje.js
@@ -46,6 +46,7 @@ module.exports = class EstudianteBO4Mensaje{
     }
 
     static validateID(data){
+        var regEx = /^[0-9a-zA-Z]+$/;
         var aprobado = (v.isLength(data,1,20) && v.value.match(regEx))
         if(!aprobado) console.log(' fallando aqui con '+ data)
         return aprobado
